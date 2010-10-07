3c8d883347e26e7672184acb8c0f3c966eee4877
Jean-Philippe Paradis <hexstream@gmail.com>
2010-10-06 20:13:09 -0400

    Rename MAPPCAN to MAPPCON.

--- build.lisp
+++ build.lisp
@@ -54,7 +54,7 @@
       :selectors (list ,@(if (form-with-operator-p selectors 'or)
 			     (mapcar #'css-build-selector (cdr selectors))
 			     (list (css-build-selector selectors))))
-      :declarations (list ,@(mappcan (lambda (property value)
+      :declarations (list ,@(mappcon (lambda (property value)
 				       (css-build-declaration property value))
 				     declarations)))))
 
